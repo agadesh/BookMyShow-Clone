1bc3ae807d2188424eaf91a47b6d528320aca5b5
Adesh Agarwal <adeshagarwal440@gmail.com>
2021-02-06 00:22:18 +0530

    changes

--- script.js
+++ script.js
@@ -15,21 +15,21 @@ carousalSlide.style.transform = 'translateX(' + -size * counter + 'px)';
 function moveNext() {
   if (counter >= carousalImages.length - 2) {
     counter = carousalImages.length - 7;
-    carousalSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+    carousalSlide.style.transform = 'translateX(' + -(size * counter + 10 * counter) + 'px)';
   }
   carousalSlide.style.transition = 'transform 1s ease-in-out';
   counter++;
-  carousalSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+  carousalSlide.style.transform = 'translateX(' + -(size * counter + 10 * counter) + 'px)';
   document.getElementsByName('radio-btn')[counter - 1].checked = true;
 }
 function movePrev() {
   if (counter <= 1) {
     counter = carousalImages.length - counter;
-    carousalSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+    carousalSlide.style.transform = 'translateX(' + -(size * counter + 10 * counter) + 'px)';
   }
   carousalSlide.style.transition = 'transform 1s ease-in-out';
   counter--;
-  carousalSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+  carousalSlide.style.transform = 'translateX(' + -(size * counter + 10 * counter) + 'px)';
   document.getElementsByName('radio-btn')[counter - 1].checked = true;
 }
 //Button Listeners
@@ -56,7 +56,7 @@ document.getElementsByName('radio-btn').forEach(elem => {
   elem.addEventListener('change', function (event) {
     clearTimeout(repeat);
     counter = event.target.value;
-    carousalSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+    carousalSlide.style.transform = 'translateX(' + -(size * counter + 10 * counter) + 'px)';
     repeat = setTimeout('changeSlide()', time);
   });
 });
