45b656be7aae42762e8981229cac50580f08c80a
Adesh Agarwal <adeshagarwal440@gmail.com>
2021-02-06 21:11:43 +0530

    adding features

---
+++ movieinfo.js
@@ -0,0 +1,28 @@
+const dateprevBtn = document.querySelector('#dateprevBtn');
+const datenextBtn = document.querySelector('#datenextBtn');
+const dateList = document.querySelector('.date-selector');
+const date = document.querySelectorAll('.date-item');
+let pos = 0;
+datenextBtn.addEventListener('click', () => {
+  if (pos >= 7) return;
+  pos++;
+  dateList.style.transform = 'translateX(' + -47 * pos + 'px)';
+});
+dateprevBtn.addEventListener('click', () => {
+  if (pos == 0) return;
+  pos--;
+  dateList.style.transform = 'translateX(' + -47 * pos + 'px)';
+});
+
+document.getElementsByName('dradio-btn').forEach((elem, i) => {
+  elem.addEventListener('change', function (event) {
+    pos = event.target.value - 1;
+    dateList.style.transform = 'translateX(' + -47 * pos + 'px)';
+    date[i].classList.toggle('di-active');
+    for (let a = 0; a < 8; a++) {
+      if (a != i) {
+        date[a].classList.remove('di-active');
+      }
+    }
+  });
+});
